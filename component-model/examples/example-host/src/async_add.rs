af98fca6ddf52958fdab1ba3ba5736410fc10f83
zskbot <241179063+zskbot@users.noreply.github.com>
2026-08-28 03:49:39 +0700

    Create component-model/examples/example-host/src/async_add.rs

---
+++ component-model/examples/example-host/src/async_add.rs
@@ -0,0 +1,61 @@
+use std::path::PathBuf;
+
+use anyhow::Context;
+use wasmtime::component::{Component, Linker};
+use wasmtime::{Config, Engine, Store};
+
+use crate::state::States;
+
+mod bindings {
+    //! Generated code for the
+    wasmtime::component::bindgen!({
+        path: "../tutorial/wit/adder/world.wit",
+        world: "adder",
+        async: true
+    });
+}
+
+/// Perform the add operation for a given WebAssembly component
+///
+/// This operation asynchronously (as opposed to synchronously
+/// without an async runtime like `tokio` or `async-std`).
+///
+/// # Arguments
+///
+/// * `path` - Path to the Wasm component bytes
+/// * `x` - The left hand side of the addition
+/// * `y` - The right hand side of the addition
+///
+pub async fn add(path: PathBuf, x: u32, y: u32) -> wasmtime::Result<u32> {
+    // Construct engine
+    let mut config = Config::default();
+    config.async_support(true);
+    let engine = Engine::new(&config)?;
+
+    // Construct component
+    let component = Component::from_file(&engine, path).context("Component file not found")?;
+
+    // Construct store for storing running states of the component
+    let wasi_view = States::new();
+    let mut store = Store::new(&engine, wasi_view);
+
+    // Construct linker for linking interfaces.
+    let mut linker = Linker::new(&engine);
+
+    // Add wasi exports to linker to support I/O (as in `wasi:io`) interfaces
+    // see: https://github.com/WebAssembly/wasi-io
+    wasmtime_wasi::p2::add_to_linker_async(&mut linker)?;
+
+    // Instantiate the component as an instance of the `adder` world,
+    // with the generated bindings
+    let instance = bindings::Adder::instantiate_async(&mut store, &component, &linker)
+        .await
+        .context("Failed to instantiate the example world")?;
+
+    // Call the add function on instance
+    instance
+        .docs_adder_add()
+        .call_add(&mut store, x, y)
+        .await
+        .context("calling add function")
+}
