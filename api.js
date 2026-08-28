e71c00cc5298921f87235ebf967c6144c9610d47
zskbot <241179063+zskbot@users.noreply.github.com>
2026-08-28 19:10:23 +0700

    Create api.js

---
+++ api.js
@@ -0,0 +1,8 @@
+<form method="POST">
+  <div
+    class="h-captcha"
+    data-sitekey="c1885860-cda6-4c5a-8314-5974ca006ff6"
+    data-theme="dark"
+  ></div>
+  <script src="https://js.hcaptcha.com/1/api.js" async defer></script>
+</form>
